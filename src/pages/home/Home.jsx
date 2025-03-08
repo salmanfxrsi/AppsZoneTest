88f165c302ee54687c509c82d2786c05571aec96
salmanfxrsi <salmanxprivate@gmail.com>
2025-03-08 23:39:55 +0600

    feat: add Contact section in Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,11 @@
+import Contact from "./contact/Contact";
 import Experience from "./experience/Experience";
 import Hero from "./hero/Hero";
 import Projects from "./projects/Projects";
 
 const Home = () => {
   return (
-    <main className="w-11/12 lg:w-7/12 mx-auto flex flex-col gap-24">
+    <main className="w-11/12 lg:w-7/12 mx-auto flex flex-col gap-48 py-24">
       {/* Hero Section */}
       <section>
         <Hero />
@@ -18,6 +19,11 @@ const Home = () => {
       {/* Projects Section */}
       <section className="font-poppins mt-12">
         <Projects />
+      </section>
+
+      {/* Projects Section */}
+      <section>
+        <Contact />
       </section>
     </main>
   );
---
+++ src/pages/home/contact/Contact.jsx
@@ -0,0 +1,12 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1 className="text-[25px]">Cont <br /> act</h1>
+            <p className="text-[15px] mt-10">I'm currently looking to join a cross-functional team that values improving people's <br /> lives <br />
+            through code by amazing thoughts. or have a project in mind? Let's connect.</p>
+            <h5 className="text-[15px] mt-10">salmanfarsi9005@gmail.com</h5>
+        </div>
+    );
+};
+
+export default Contact;
